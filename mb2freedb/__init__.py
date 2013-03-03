f988cc757f651a7d492baa24c3d274c103f77afe
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-03-03 13:22:07 -0700

    Bump version number in light of my past changes.

--- mb2freedb/__init__.py
+++ mb2freedb/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2011 Lukas Lalinsky
+# Copyright (C) 2011 Lukas Lalinsky, 2012 MetaBrainz Foundation
 # Distributed under the MIT license, see the LICENSE file for details.
 
-__version__ = "0.9.0"
+__version__ = "0.9.1"
